41faa788ab6c5f41035486518b594e3a2ba82b7a
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2023-01-14 16:55:20 -0300

    Add database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
+import os
 from flask import Flask
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASS')
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY=SECRET_KEY,
+        DB_SERVER=DB_HOST,
+    )
 
     @app.route('/')
     def hello_geek():
