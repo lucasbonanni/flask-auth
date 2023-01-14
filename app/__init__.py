86c5f43368a42d3c02d0757f13ddf3aabb0a2841
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2023-01-14 16:42:50 -0300

    Refactor flask init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route('/')
+    def hello_geek():
+        return '<h1>Hello from Flask & Docker</h2>'
+
+    return app
 
 if __name__ == "__main__":
-    app.run()
+    create_app().run()
